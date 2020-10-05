e69a0206125576acae7a106b510e2334be188fd8
Rodolfo D. Arce S <info@rodolfoarce.com>
2020-10-05 18:28:03 -0300

    Tarea04

---
+++ Tarea04/tarea04.sql
@@ -0,0 +1,9 @@
+-- Crear usuarios
+
+CREATE USER 'rodolfo'@'localhost' IDENTIFIED BY 'Mi_Password-es-123';
+
+CREATE USER 'usuario2'@'localhost' IDENTIFIED BY 'Su-PASS_es_456';
+
+CREATE USER 'administrador'@'%' IDENTIFIED BY 'mEgA_password_105';
+
+DROP USER 'usuario2'@'localhost';
